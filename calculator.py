d741f8acd3924a2d83878596fff149daa9bac570
emilywong <e.wong@ufl.edu>
2025-04-09 15:10:47 -0400

    first commit

--- calculator.py
+++ calculator.py
@@ -22,12 +22,5 @@ def divide(a, b):
         raise ZeroDivisionError
     return b / a
 
-def logarithm(a, b):
-    if a <= 0 or a == 1:
-        raise ValueError # value error
-    if b <= 0:
-        raise ValueError
-    return math.log(b, a)
-
 def exponent(a, b):
     return a ** b
