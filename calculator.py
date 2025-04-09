915182018183da315d1f9b99ee8842a855de8915
LiaIssakov <issakovl@ufl.edu>
2025-04-09 15:29:53 -0400

    changed function names

--- calculator.py
+++ calculator.py
@@ -11,23 +11,23 @@ import math
 def add(a, b): 
     return a + b
 
-def subtract(a, b):
+def subt(a, b):
     return a - b
 
-def multiply(a, b):
+def mul(a, b):
     return a * b
 
-def divide(a, b):
+def div(a, b):
     if a == 0:
         raise ZeroDivisionError
     return b / a
 
-def logarithm(a, b):
+def log(a, b):
     if a <= 0 or a == 1:
         raise ValueError # value error
     if b <= 0:
         raise ValueError # value error
     return math.log(b, a)
 
-def exponent(a, b):
+def exp(a, b):
     return a ** b
